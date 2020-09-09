5cf6cefa1e601dbaf893e3d1e11726e2f7cd1ed1
mauricio1798 <70928418+mauricio1798@users.noreply.github.com>
2020-09-09 16:51:31 -0500

    Agregando clase paciente

---
+++ PacienteProyecto/Paciente.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PacienteProyecto
+{
+    public class Paciente
+    {
+        public string Nombre { set; get; }
+        public string Apellido { set; get; }
+        public int Edad { set; get; }
+        public string FechaNacimiento { set; get; }
+        public string CI { set; get; }
+        public string Genero { set; get; }
+        public int NumHistoriaClinica { set; get; }
+        public int NumSala { set; get; }
+
+        public Paciente()
+        {
+
+        }
+
+        public Paciente(string nombre, string apellido, int edad, string fechaNacimiento, string cI, string genero, int numHistoriaClinica, int numSala)
+        {
+            Nombre = nombre;
+            Apellido = apellido;
+            Edad = edad;
+            FechaNacimiento = fechaNacimiento;
+            CI = cI;
+            Genero = genero;
+            NumHistoriaClinica = numHistoriaClinica;
+            NumSala = numSala;
+        }
+    }
+}
