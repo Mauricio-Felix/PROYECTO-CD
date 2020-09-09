347f932b255cfb9cc22dffda75f19c3c1b6dd492
erick184 <70928437+erick184@users.noreply.github.com>
2020-09-09 17:50:39 -0500

    codificando forms 1

--- PacienteProyecto/Form1.cs
+++ PacienteProyecto/Form1.cs
@@ -12,11 +12,61 @@ namespace PacienteProyecto
 {
     public partial class frmRegistro : Form
     {
+        Pacientes personas = new Pacientes();
         public frmRegistro()
         {
             InitializeComponent();
         }
 
-       
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            try 
+            {
+                if(VerificarInformacion())
+                {
+                    Paciente persona = new Paciente();
+                    persona.Nombre = mtxtNombres.Text;
+                    persona.Apellido = mtxtApellidos.Text;
+                    persona.CI = txtCedula.Text;
+                    persona.Edad = Convert.ToInt32(mtxtEdad.Text);
+                    persona.FechaNacimiento = dtpFechaNacimiento.Value.ToString();
+                    persona.NumHistoriaClinica = Convert.ToInt32(mtxtNumHistoria.Text);
+                    persona.NumSala = Convert.ToInt32(mtxtSala.Text);
+                    persona.Genero = cmbGenero.Text;
+                    personas.Agregar(persona);
+                    MessageBox.Show("Datos guardados correctamente");
+                    LimpiarControles();
+                }
+                else
+                {
+                    MessageBox.Show("Ingrese toda la informacion");
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+        public bool VerificarInformacion()
+        {
+            bool verificar = false;
+            if(mtxtApellidos.Text!=""&& mtxtNombres.Text!=""&& mtxtEdad.Text!=""&& mtxtNumHistoria.Text!=""&& mtxtSala.Text!=""&& txtCedula.Text!=""  )
+            {
+                verificar = true;
+            }
+            return verificar;
+        }
+        public void LimpiarControles()
+        {
+            mtxtApellidos.Text = "";
+            mtxtNombres.Text = "";
+            mtxtEdad.Text = "";
+            mtxtNumHistoria.Text = "";
+            mtxtSala.Text = "";
+            txtCedula.Text = "";
+            cmbGenero.SelectedValue = false;
+            dtpFechaNacimiento.Value = DateTime.Now;
+        }
     }
 }
