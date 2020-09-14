fe51b8c248217d112dc531ca2069de1880b262a9
mauricio1798 <70928418+mauricio1798@users.noreply.github.com>
2020-09-13 19:08:12 -0500

    Agregando clase prueba

---
+++ PacienteProyecto/ClasePrueba.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PacienteProyecto
+{
+    public class ClasePrueba
+    {
+        public int Num1 { set; get; }
+    }
+}
